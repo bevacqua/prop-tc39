6caa1d94f5fd2588da7192beb1764f31bba10ff0
t1ger@zhihu.com <t1ger@zhihu.com>
2017-12-04 11:50:12 +0800

    :bug: fix: finished proposals

--- proposals.js
+++ proposals.js
@@ -49,32 +49,44 @@ function fetch(resource, parse, done) {
   }
 }
 
+function parseStageTable($, table, stage) {
+  return $(table)
+    .find('tbody tr')
+    .toArray()
+    .map(tr => {
+      const $tr = $(tr)
+      const readyToAdvance = $tr.find('td:nth-child(1)').text().trim() === '🚀'
+      const $title = $tr.find('td:nth-child(2)')
+      const $titleLink = $title.find('a')
+      const titleHtml = ($titleLink.length ? $titleLink : $title).html().trim()
+      const href = $titleLink.attr('href')
+      const champions = $tr.find('td:nth-child(3)').text().trim().split(rchampionseparator)
+      const proposalMeta = {
+        titleHtml,
+        href,
+        stage,
+        champions,
+        readyToAdvance
+      }
+      
+      // finished proposals only
+      if (stage === 4) {
+        const meeting = moment.utc($tr.find('td:nth-child(3)').text().trim(), 'MMMM YYYY')
+        const meetingHref = $tr.find('td:nth-child(3) a').attr('href')
+        const publicationYear = parseInt($tr.find('td:nth-child(4)').text().trim())
+        proposalMeta.meeting = meeting
+        proposalMeta.meetingHref = meetingHref
+        proposalMeta.publicationYear = publicationYear
+      }
+      return proposalMeta
+    })
+}
+
 function parseActive(html) {
   const $ = cheerio.load(html)
   const proposals = $('#readme table')
     .toArray()
-    .map((table, i) => {
-      const stage = 3 - i // stage 3 -> stage 1
-      return $(table)
-        .find('tbody tr')
-        .toArray()
-        .map(tr => {
-          const $tr = $(tr)
-          const readyToAdvance = $tr.find('td:nth-child(1)').text().trim() === '🚀'
-          const $title = $tr.find('td:nth-child(2)')
-          const $titleLink = $title.find('a')
-          const titleHtml = ($titleLink.length ? $titleLink : $title).html().trim()
-          const href = $titleLink.attr('href')
-          const champions = $tr.find('td:nth-child(3)').text().trim().split(rchampionseparator)
-          return {
-            titleHtml,
-            href,
-            stage,
-            champions,
-            readyToAdvance
-          }
-        })
-    })
+    .map((table, index) => parseStageTable($, table, 3 - index))
   
   return sortProposals(
     flatten(proposals)
@@ -82,37 +94,16 @@ function parseActive(html) {
 }
 
 function parseProposed(html) {
-  return parseActive(html) // for now TC39 has the same table schema for active proposals and stage 0 proposals
+  const $ = cheerio.load(html)
+  return sortProposals(
+    parseStageTable($, $('#readme table').eq(0), 0)
+  )
 }
 
 function parseFinished(html) {
   const $ = cheerio.load(html)
   return sortProposals(
-    $('#readme table')
-      .eq(0)
-      .find('tbody tr')
-      .toArray()
-      .map(tr => {
-        const $tr = $(tr)
-        const $title = $tr.find('td:nth-child(1)')
-        const $titleLink = $title.find('a')
-        const titleHtml = ($titleLink.length ? $titleLink : $title).html().trim()
-        const href = $titleLink.attr('href')
-        const champions = $tr.find('td:nth-child(2)').text().trim().split(rchampionseparator)
-        const meeting = moment.utc($tr.find('td:nth-child(3)').text().trim(), 'MMMM YYYY')
-        const meetingHref = $tr.find('td:nth-child(3) a').attr('href')
-        const publicationYear = parseInt($tr.find('td:nth-child(4)').text().trim())
-        const stage = 4
-        return {
-          titleHtml,
-          href,
-          stage,
-          champions,
-          meeting,
-          meetingHref,
-          publicationYear
-        }
-      })
+    parseStageTable($, $('#readme table').eq(0), 4)
   )
 }
 
